dd733b9c421536aac1e82bc298fbd37da9b0dcb4
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-11-29 09:54:22 +0530

    bitwiseOperator
    update

--- Basics/Basics2/3.BitwiseOperatos/1.ShiftOperators/main.cpp
+++ Basics/Basics2/3.BitwiseOperatos/1.ShiftOperators/main.cpp
@@ -5,10 +5,21 @@ int main(){
     unsigned short int data {0xff12u};
     std::cout<<"data (bin) : "<<std::bitset<16>(data)<<std::endl;
     std::cout<<"shifting right >> "<<std::endl;
+    //right shift by one
     data = static_cast<unsigned short int>(data >> 1);
     std::cout<<"data (bin) : "<<std::bitset<16>(data)<<std::endl;
-
-
-
+    std::cout<<"shifting left >> "<<std::endl;
+    //left shift by one
+    data = static_cast<unsigned short int>(data << 1);
+    std::cout<<"data (bin) : "<<std::bitset<16>(data)<<std::endl;
+    std::cout<<"shifting right by three>> "<<std::endl;
+    //right shift by three
+    data = static_cast<unsigned short int>(data >> 3);
+    std::cout<<"data (bin) : "<<std::bitset<16>(data)<<std::endl;
+    std::cout<<"shifting left by two>> "<<std::endl;
+    //left shift by two
+    data = static_cast<unsigned short int>(data << 2);
+    std::cout<<"data (bin) : "<<std::bitset<16>(data)<<std::endl;
+    
     return 0;
 }
