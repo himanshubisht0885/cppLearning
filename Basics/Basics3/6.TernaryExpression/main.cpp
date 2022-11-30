41a982eebac0598ceee76cdf2144ed75ae5f2f76
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-11-30 11:52:56 +0530

    ternaryExpression

--- Basics/Basics3/6.TernaryExpression/main.cpp
+++ Basics/Basics3/6.TernaryExpression/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
-consteval int get_value(){
-    return 3;
-}
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    int max;
+    int a {34};
+    int b {50};
+    max = ( a > b )? a : b ;
+    std::cout<<max<<std::endl;
+    bool b1 {false};
+    max = (b1)? b:a;
+    std::cout<<max<<std::endl;
+    max = (b1)? b:2.2l;
+    std::cout<<max<<std::endl;
     return 0;
 }
