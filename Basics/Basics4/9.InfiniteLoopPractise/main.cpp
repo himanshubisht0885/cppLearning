ee78b0fa9c265f8f4185a4a4d33f0d4c30bd9ada
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-01 21:54:38 +0530

    infinitelooppractise

---
+++ Basics/Basics4/9.InfiniteLoopPractise/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    //infinite for loop there're many variations
+    /*for (size_t i {0}; ;++i){
+        std::cout<<"I love C++"<<std::endl;
+    }*/
+    /*for(; true;){
+        std::cout<<"I love C++"<<std::endl;
+    }*/
+    //infinite while loop
+    while(true){
+         std::cout<<"I love C++"<<std::endl;
+    }
+    
+    
+    return 0;
+}
