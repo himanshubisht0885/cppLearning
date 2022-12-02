3615480558a02e03ae26bd37b45b16a1af792cf6
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-02 22:44:12 +0530

    infinite loop

--- Basics/Basics4/9.InfiniteLoopPractise/main.cpp
+++ Basics/Basics4/9.InfiniteLoopPractise/main.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
+#include <iomanip>
 
+void infiloop(){
+    char k {'Y'};
+    do{
+        char k {'Y'};
+        int operand1;
+        int operand2;
+        std::cout<<"Enter value of operand1 : "<<std::endl;
+        std::cin>>operand1;
+        std::cout<<std::endl;
+        std::cout<<"Enter value of operand2 : "<<std::endl;
+        std::cin>>operand2;
+        std::cout<<std::endl;
+        unsigned char operator1;
+        std::cout<<"Enter operator +-*/"<<std::endl;
+        std::cin>>operator1;
+        std::cout<<std::endl;
+        switch (operator1){
+            case '*':{
+                std::cout<<operand1*operand2<<std::endl;
+            }
+            break;
+            case '+':{
+                std::cout<<operand1+operand2<<std::endl;
+            }
+            break;
+            case '-':{
+                std::cout<<operand1-operand2<<std::endl;
+            }
+            break;
+            case '/':{
+                std::cout<<operand1/operand2<<std::endl;
+            }
+            break;
+            default :{
+                std::cout<<"enter correct value "<<std::endl;
+            }
+            break;
+        }
+        std::cout<<"Press Y to continue : "<<std::endl;
+        std::cin>>k;
+        unsigned int z = static_cast<int>(k);
+    } while(z==89);
+}
 int main(){
-    //infinite for loop there're many variations
-    /*for (size_t i {0}; ;++i){
-        std::cout<<"I love C++"<<std::endl;
-    }*/
-    /*for(; true;){
-        std::cout<<"I love C++"<<std::endl;
-    }*/
-    //infinite while loop
-    while(true){
-         std::cout<<"I love C++"<<std::endl;
-    }
-    
-    
+    infiloop();
     return 0;
 }
