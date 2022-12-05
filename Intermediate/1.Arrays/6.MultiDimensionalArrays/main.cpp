1fb07a6153c3896b697765ca65c0e64c4b5e6799
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-05 09:13:27 +0530

    multiDeminesionalArray
    omitting values

--- Intermediate/1.Arrays/6.MultiDimensionalArrays/main.cpp
+++ Intermediate/1.Arrays/6.MultiDimensionalArrays/main.cpp
@@ -32,5 +32,30 @@ int main(){
         }
     }
 
+    const size_t num_cols {3};
+    //omitting dimensions out , only columns    
+    int arr2 [] [ num_cols ]{
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        {10,11,12},
+        {100,110,120}
+        };
+    for (size_t i {}; i<sizeof(arr2)/sizeof(arr2)[0];++i){
+        for (size_t j {}; j<sizeof(arr2[0])/sizeof(arr2)[0][0]; ++j){
+            std::cout<<"value of element at ["<<i<<"]"<<"["<<j<<"] : "<<arr2[i][j]<<std::endl;
+
+        }
+    }
+    int arr3 [] [3] [2]{
+        {{1,2},{3,4},{5,6}},
+        {{7,8},{9,10},{11,12}}
+    };
+    for(size_t i{};i<sizeof(arr3)/sizeof(arr3[0]);++i){
+        for (size_t j{}; j<sizeof(arr3[0])/sizeof(arr3[0][0]);++j){
+            for (size_t z {}; z<sizeof(arr3[0][0])/sizeof(arr3)[0][0][0];++z)
+                std::cout<<"value of element at ["<<i<<"]"<<"["<<j<<"]"<<"["<<z<<"] : "<<arr3[i][j][z]<<std::endl;
+        }
+    }
     return 0;
 }
