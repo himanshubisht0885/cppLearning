757723a6e7ad654f89aafc3ea102fb2938b72d55
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-06 12:55:01 +0530

    pointersChar

--- Intermediate/2.Pointers/2.PoointerToChar/main.cpp
+++ Intermediate/2.Pointers/2.PoointerToChar/main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 
 int main(){
-    int a {2124};
-    int *p_number {&a}; //can only store address of a vaiable of type int
-    double *q_number {}; //can only store address of a vaiable of type double
-    //eplicitely initialize to nullptr
-    int *a_number {nullptr};
-    double *b_number {nullptr};
-    std::cout<<"a pointer : "<<a_number<<std::endl;
-    std::cout<<"b pointer : "<<b_number<<std::endl;
-    std::cout<<"size of pointer a : "<<sizeof(a_number)<<std::endl;
-    std::cout<<"value of a : "<<std::endl;
-    std::cout<<"value of p : "<<p_number<<"size of pointer p : "<<sizeof(p_number)<<std::endl;
-    a =1313;
-    p_number = &a;
-    std::cout<<"value of a : "<<std::endl;
-    std::cout<<"value of p : "<<p_number<<"size of pointer p : "<<sizeof(p_number)<<std::endl;
+   char *p{};
+   char a {'A'};
+   p = &a;
+   const char *q {"hello"};
+   std::cout<<"value of q : "<<q<<std::endl;
+   std::cout<<"value of *q : "<<*q<<std::endl;
     return 0;
 }
