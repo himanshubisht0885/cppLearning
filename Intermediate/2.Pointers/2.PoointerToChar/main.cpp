0ede17f25a6031172e66f76442eae027d2c73094
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-06 17:56:49 +0530

    pointertochar
    special property of pointer of c -string

--- Intermediate/2.Pointers/2.PoointerToChar/main.cpp
+++ Intermediate/2.Pointers/2.PoointerToChar/main.cpp
@@ -7,5 +7,9 @@ int main(){
    const char *q {"hello"};
    std::cout<<"value of q : "<<q<<std::endl;
    std::cout<<"value of *q : "<<*q<<std::endl;
+   //special properties of pointers with string literals
+   const char *z = {"hello"};
+   std::cout<<"value of z : "<<z<<std::endl;
+   std::cout<<"value of *z : "<<*z<<std::endl; //return first liiter of string
     return 0;
 }
