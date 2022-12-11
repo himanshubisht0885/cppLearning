5e512e9e6868b085c483ea32f2645f8b7d6b9b0e
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-11 22:28:45 +0530

    pointerconst

--- Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
+++ Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
 int main(){
-   const char * p [] {
-    "hello",
-    "abc",
-    "xyz"
-   };
-   std::cout<<p<<std::endl;
-   std::cout<<*p<<std::endl;
-
+    int var1 {5}; //not constant, can change nuber 
+    std::cout<<"number var1 : "<<var1<<std::endl;
+    std::cout<<"number &var1 : "<<&var1<<std::endl;
+    //modify
+    var1 = 12;
+    var1 += 4;
+    std::cout<<"after modifying -------"<<std::endl;
+    std::cout<<"number var1 : "<<var1<<std::endl;
+    std::cout<<"number &var1 : "<<&var1<<std::endl;
     return 0;
 }
