44770de415b82cac9e44f19022602af5092949e7
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-14 23:03:26 +0530

    Update main.cpp

--- Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
+++ Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
+void camprtain(){
+    short unsigned int x {3};
+    std::cout<<"value of x : "<<x<<std::endl;
+}
+
+
+
 int main(){
-    int var1 {5}; //not constant, can change nuber 
-    std::cout<<"number var1 : "<<var1<<std::endl;
-    std::cout<<"number &var1 : "<<&var1<<std::endl;
-    //modify
-    var1 = 12;
-    var1 += 4;
-    std::cout<<"after modifying -------"<<std::endl;
-    std::cout<<"number var1 : "<<var1<<std::endl;
-    std::cout<<"number &var1 : "<<&var1<<std::endl;
+    const char * k []{
+        "wfg",
+        "fer",
+        "fwgeb"
+    };
+    short unsigned int k {32};
+    int * const k 
     return 0;
 }
