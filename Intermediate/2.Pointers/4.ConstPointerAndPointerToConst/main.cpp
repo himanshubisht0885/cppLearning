c029dd6268f835f42a90a4cea213873f17accc1a
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-15 09:24:35 +0530

    non const pointer to non const data

--- Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
+++ Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 
-void camprtain(){
-    short unsigned int x {3};
-    std::cout<<"value of x : "<<x<<std::endl;
-}
-
-
-
 int main(){
-    const char * k []{
-        "wfg",
-        "fer",
-        "fwgeb"
-    };
-    short unsigned int k {32};
-    int * const k 
+    //raw variable that can be modified
+    std::cout<<"raw variable that can be modified --------"<<std::endl;
+    std::cout<<"variable that can be modified----"<<std::endl;
+    int k {4};
+    std::cout<<"value : "<<k<<std::endl;
+    std::cout<<"&value : "<<&k<<std::endl;
+    k += 6;
+    std::cout<<"modifying variable-----"<<std::endl;
+    std::cout<<"value : "<<k<<std::endl;
+    std::cout<<"&value : "<<&k<<std::endl; //same location
+
+    //non const pointer to non const data
+    std::cout<<"non const pointer to non const data-----"<<std::endl;
+    int *p {nullptr};
+    int n1 {40};
+    std::cout<<"value of p : "<<p<<std::endl;
+    std::cout<<"value of *p : "<<*p<<std::endl;
+
+
     return 0;
 }
