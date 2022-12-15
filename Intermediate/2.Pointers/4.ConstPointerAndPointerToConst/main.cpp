8db3dafc7fa3a01e1115034c44976f643aa113b3
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-15 09:32:55 +0530

    new

--- Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
+++ Intermediate/2.Pointers/4.ConstPointerAndPointerToConst/main.cpp
@@ -17,7 +17,25 @@ int main(){
     int *p {nullptr};
     int n1 {40};
     std::cout<<"value of p : "<<p<<std::endl;
+    //std::cout<<"value of *p : "<<*p<<std::endl;
+    std::cout<<"---------------"<<std::endl;
+
+    p=&n1;
+    std::cout<<"value of p : "<<p<<std::endl;
     std::cout<<"value of *p : "<<*p<<std::endl;
+    std::cout<<"---------------"<<*p<<std::endl;
+    *p = 432; //change the pointed value through pointer
+    std::cout<<"modifying *p----"<<std::endl;
+    std::cout<<"value of n1 : "<<n1<<std::endl;
+    std::cout<<"value of p : "<<p<<std::endl;
+    std::cout<<"value of *p : "<<*p<<std::endl;
+    std::cout<<"---------------"<<*p<<std::endl;
+    int n2 {20};
+    p=&n2; //change the pinter itself to make it pointed somewhere else
+    std::cout<<"value of p : "<<p<<std::endl;
+    std::cout<<"value of *p : "<<*p<<std::endl;
+    std::cout<<"---------------"<<*p<<std::endl;
+
 
 
     return 0;
